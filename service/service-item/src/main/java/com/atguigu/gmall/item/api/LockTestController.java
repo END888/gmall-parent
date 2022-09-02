9d55f7687d22c472968f07fa5f4d68188da1386a
ycy <247208124@qq.com>
2022-09-02 22:02:41 +0800

    feature: Elasticsearch search 练习完成

--- service/service-item/src/main/java/com/atguigu/gmall/item/api/LockTestController.java
+++ service/service-item/src/main/java/com/atguigu/gmall/item/api/LockTestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
-
+// 1
 @RestController
 @RequestMapping("/lock")
 public class LockTestController {
